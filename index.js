f0dafe0224b22c259c57644ed942b850cf81adca
monish <monish2304@gmail.com>
2024-06-30 23:05:29 +0530

    wrote controllers and routes for auth

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-import dotenv from "dotenv"
-import cors from 'cors'
+import dotenv from 'dotenv';
+import cors from 'cors';
+import express from 'express';
+import connectDb from './Connections/dbconnections.js';
+import routes from './routes/index.js';
+import cookieParser from 'cookie-parser';
+dotenv.config();
 
-import express from "express";
-import connectDb from "./Connections/dbconnections.js"
-
-dotenv.config()
-
-const port = process.env.PORT || 4000
-const app=express();
+const port = process.env.PORT || 4000;
+const app = express();
 app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.send("Hello");
-})
+app.use('/api', routes);
 
 connectDb();
 app.listen(port, () => {
